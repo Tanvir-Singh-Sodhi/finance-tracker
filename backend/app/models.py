61647ce00ae2666a09efd827b8e2a51a00b8d066
psyts23 <psyts23@nottingham.ac.uk>
2026-09-10 00:34:43 +0100

    feat: added account model with sqlalchemy

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,31 @@
 # this is where i expect to make the final ERD to sqlA conversion
+# going to work on 'Account' table
+
+import enum
+
+from sqlalchemy import Column, Enum, Integer, Numeric, String, create_engine
+from sqlalchemy.orm import declarative_base
+
+# using sqlite to create a local db file
+db_url = "sqlite:///./finance.db"
+
+engine = create_engine(db_url)
+base = declarative_base()
+
+
+# now we can actually make a table called 'Account'
+class AccountType(enum.Enum):
+    CURRENT = "current"
+    SAVINGS = "savings"
+
+
+class Account(base):
+    __tablename__ = "accounts"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    bank_name = Column(String)
+    account_type = Column(Enum(AccountType))
+    initial_balance = Column(Numeric)
+
+
+base.metadata.create_all(engine)
