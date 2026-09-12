1d49bc40c84dbcd64dd46597012a2bb785ba17a4
psyts23 <psyts23@nottingham.ac.uk>
2026-09-12 23:39:35 +0100

    feat: added all models from ERD

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,7 +3,17 @@
 
 import enum
 
-from sqlalchemy import Column, Enum, Integer, Numeric, String, create_engine
+from sqlalchemy import (
+    Boolean,
+    Column,
+    DateTime,
+    Enum,
+    ForeignKey,
+    Integer,
+    Numeric,
+    String,
+    create_engine,
+)
 from sqlalchemy.orm import declarative_base
 
 # using sqlite to create a local db file
@@ -22,10 +32,40 @@ class AccountType(enum.Enum):
 class Account(base):
     __tablename__ = "accounts"
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    bank_name = Column(String)
-    account_type = Column(Enum(AccountType))
-    initial_balance = Column(Numeric)
+    name = Column(String, nullable=False)
+    bank_name = Column(String, nullable=False)
+    account_type = Column(Enum(AccountType), nullable=False)
+    initial_balance = Column(Numeric, nullable=False)
+
+
+class Category(base):
+    __tablename__ = "categories"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+
+class RecurringExpense(base):
+    __tablename__ = "recurring_expenses"
+    id = Column(Integer, primary_key=True)
+    category_id = Column(ForeignKey("categories.id"), nullable=False)
+    name = Column(String, nullable=False)
+    amount = Column(Numeric, nullable=False)
+    frequency = Column(String, nullable=False)
+    next_due_date = Column(DateTime, nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+
+
+class Transaction(base):
+    __tablename__ = "transactions"
+    id = Column(Integer, primary_key=True)
+    source_account_id = Column(ForeignKey("accounts.id"), nullable=False)
+    destination_account_id = Column(ForeignKey("accounts.id"))
+    category_id = Column(ForeignKey("categories.id"))
+    recurring_expense_id = Column(ForeignKey("recurring_expenses.id"))
+    transaction_type = Column(String, nullable=False)
+    date = Column(DateTime, nullable=False)
+    description = Column(String)
+    amount = Column(Numeric, nullable=False)
 
 
 base.metadata.create_all(engine)
